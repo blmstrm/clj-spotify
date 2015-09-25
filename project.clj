a2ad301eb82128bd6c6c6b02691f934b50752b89
Kalle Blomström <kalle.blomstrom@gmail.com>
2015-09-26 00:56:42 +0200

    Adds MIT license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-spotify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A client library for the Spotify Web API"
+  :url "https://github.com/blmstrm/clj-spotify"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
