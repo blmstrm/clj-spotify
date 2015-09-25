62c3147958f364b27ad720d6d6eb87073a2a8d8a
Kalle Blomström <kalle.blomstrom@gmail.com>
2015-09-26 00:12:46 +0200

    Bumped clojure dependency to 1.7. Removed old dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-ring "0.9.4"]]
-  :ring {:handler clj-spotify.auth/app}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/tools.logging "0.3.1"]
                  [clj-http "1.1.2"]
-                 [org.apache.logging.log4j/log4j-core "2.2"]
                  ])
