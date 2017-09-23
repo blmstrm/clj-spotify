84aac5949f88f1c07cbf3f01086cf2b5c90d64bb
Kalle Blomström <kalle@amuse.io>
2017-09-23 11:03:48 +0200

    Updates old dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/blmstrm/clj-spotify"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :plugins [[lein-cloverage "1.0.6"]]
+  :plugins [[lein-cloverage "1.0.9"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
-                 [cheshire "5.6.1"]
-                 [clj-http "3.1.0"]])
+                 [cheshire "5.8.0"]
+                 [clj-http "3.7.0"]])
