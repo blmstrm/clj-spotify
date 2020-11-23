fac2090a4cb59fcdd5ce54a3abc73ebe12080601
Sergio Morales <sergio@backbase.com>
2020-11-23 01:33:00 +0100

    bumps dependencies and version to 0.1.10

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject clj-spotify "0.1.9"
+(defproject clj-spotify "0.1.10"
   :description "A client library for the Spotify Web API"
   :url "https://github.com/blmstrm/clj-spotify"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :plugins [[lein-cloverage "1.0.13"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]
+  :plugins [[lein-cloverage "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.codec "0.1.1"]
-                 [cheshire "5.8.1"]
-                 [clj-http "3.9.1"]]
+                 [cheshire "5.10.0"]
+                 [clj-http "3.11.0"]]
   :profiles {
              :dev [:project/dev]
              :test [:project/dev :profiles/test]
              :project/dev {
                            :source-paths ["dev-resources"]
-                           :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                           :dependencies [[org.clojure/tools.namespace "1.0.0"]
                                           [loudmoauth "0.1.3"]
-                                          [ring "1.7.0"]
+                                          [ring "1.8.2"]
                                           [org.clojure/tools.nrepl "0.2.13"]]}
              :profiles/test {}})
